b5a09e5ef8c8fd20aa74311196c22a75d9c11b3e
Sneha Priscilla Makini <sneha@Snehas-MacBook-Pro.local>
2016-12-01 14:27:49 -0500

    Initial commit with basic pacman

---
+++ Assets/Scripts/spawnFood.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnFood : MonoBehaviour {
+
+	public GameObject foodPrefab;
+	public Sprite[] foodSprites;
+
+	public void MakeRandomFood(){
+		int arrayIdx = Random.Range(0, foodSprites.Length);
+		Sprite foodSprite =  foodSprites[arrayIdx];
+		GameObject newFood = Instantiate(foodPrefab);
+		newFood.GetComponent<SpriteRenderer>().sprite = foodSprite;
+	}
+
+	// Use this for initialization
+	void Start() {
+		
+		MakeRandomFood();
+	}
+
+	void Awake(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
